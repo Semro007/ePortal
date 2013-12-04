50e5f6b88e1e131060e4a5ce003f05f1b43596e6
Sergio Martinez Roig <sergio@iMac-de-Sergio.local>
2013-12-04 22:49:28 +0100

    Pantalla login

---
+++ ePortal/loginViewController.h
@@ -0,0 +1,13 @@
+//
+//  loginViewController.h
+//  ePortal
+//
+//  Created by Sergio Martinez Roig on 04/12/13.
+//  Copyright (c) 2013 ToSeWare Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface loginViewController : UIViewController
+
+@end
---
+++ ePortal/loginViewController.m
@@ -0,0 +1,38 @@
+//
+//  loginViewController.m
+//  ePortal
+//
+//  Created by Sergio Martinez Roig on 04/12/13.
+//  Copyright (c) 2013 ToSeWare Ltd. All rights reserved.
+//
+
+#import "loginViewController.h"
+
+@interface loginViewController ()
+
+@end
+
+@implementation loginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
