bf56d3a233f3b5ee576bc87c266c86f7d84a3400
Sergio Martinez Roig <sergio@iMac-de-Sergio.local>
2013-12-05 00:25:41 +0100

    Login 2

---
+++ ePortal/calendarioViewController.m
@@ -0,0 +1,38 @@
+//
+//  calendarioViewController.m
+//  ePortal
+//
+//  Created by Sergio Martinez Roig on 04/12/13.
+//  Copyright (c) 2013 ToSeWare Ltd. All rights reserved.
+//
+
+#import "calendarioViewController.h"
+
+@interface calendarioViewController ()
+
+@end
+
+@implementation calendarioViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
